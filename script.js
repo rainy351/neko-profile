5f70c06ee1f534bbc058b39e19b2179ef4d2489b
rainy351 <121498724+rainy351@users.noreply.github.com>
2026-04-08 18:00:06 +0300

    better player & retro range

--- script.js
+++ script.js
@@ -215,3 +215,46 @@ document.getElementById("menuItemGuestbook").addEventListener("click", () => {
 });
 
 updateTaskbarStatus();
+
+const audio = document.getElementById("musicPlayer");
+const progressContainer = document.querySelector(".progress-container");
+const progressBarFill = document.getElementById("progressBarFill");
+
+audio.addEventListener("timeupdate", () => {
+  if (audio.duration) {
+    const percent = (audio.currentTime / audio.duration) * 100;
+    progressBarFill.style.width = percent + "%";
+  }
+});
+
+progressContainer.addEventListener("click", (e) => {
+  if (!audio.duration) return;
+
+  const rect = progressContainer.getBoundingClientRect();
+  const clickX = e.clientX - rect.left;
+  const width = rect.width;
+
+  audio.currentTime = (clickX / width) * audio.duration;
+});
+
+document.getElementById("playBtn").addEventListener("click", () => {
+  audio.play().catch((e) => console.log("Браузер заблокировал автоплей: " + e));
+  document.getElementById("clickSound").play();
+});
+
+document.getElementById("pauseBtn").addEventListener("click", () => {
+  audio.pause();
+  document.getElementById("clickSound").play();
+});
+
+const loopBtn = document.getElementById("loopBtn");
+
+loopBtn.addEventListener("click", () => {
+  audio.loop = !audio.loop;
+
+  loopBtn.innerText = audio.loop ? "Loop: ON ‎" : "Loop: OFF";
+
+  loopBtn.classList.toggle("active-loop");
+
+  document.getElementById("clickSound").play();
+});
